cfb8509e440264c3caaefa7d46d6ce4607d070fa
Maryia Mayorava <mayorava.maryia@gmail.com>
2022-12-06 16:59:28 +0100

    Add HW_Cycle_While

---
+++ 18/001_Cycle_While/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_Cycle_While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Напишите программу, в которой получите целое число и используя циклическую
+конструкцию выведите на экран консоли все числа от введенного и до нуля
+включительно */
+
+            Console.WriteLine("Enter numbers: ");
+
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            while (a > 0)
+            {
+                Console.WriteLine(a--);
+            }
+            Console.WriteLine(a);
+
+        }
+    }
+}
---
+++ 18/002_Cycle_While/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _002_Cycle_While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Напишите программу, в которой получите от пользователя два целых числа.
+Используя цикл выведите все нечетные числа, расположенные между введенными
+числами. */
+            Console.WriteLine("Enter two numbers: ");
+
+            int
+                a = Convert.ToInt32(Console.ReadLine()),
+                b = Convert.ToInt32(Console.ReadLine());
+
+            while (a <= b)
+            {
+                if (a % 2 != 0)
+                {
+                    Console.WriteLine(a);
+                }
+                a++;
+             }
+            while (a >= b)
+            {
+                if ((a % 2) != 0)
+                {
+                    Console.WriteLine(a);
+                }
+                a--;
+            }
+
+        }
+    }
+}
