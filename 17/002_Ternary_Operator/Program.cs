eebad8d2d5c09628181e3918c53cd1a9c71088ec
Maryia Mayorava <mayorava.maryia@gmail.com>
2022-12-06 13:36:37 +0100

    add HW "ternary operator"

---
+++ 17/002_Ternary_Operator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _002_Ternary_Operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* 1. Получите от пользователя два целых числа. 
+             * 2. Используя выражение, в котором тернарный оператор располагается слева от 
+оператора присвоения, выполните проверку на то, что первое введенное число 
+больше или равно второму. 
+            3. (в том же выражении) Если проверка возвращает true – первому числу должно 
+присвоится значение 100, иначе 100 должно присвоится второму введенному 
+числу.
+            4. Выведите информацию о числах и их итоговых значениях на экран консоли.
+ */
+            Console.WriteLine("Enter your numbers: ");
+
+            int x, y, result;
+
+            x = Convert.ToInt32(Console.ReadLine());
+            y = Convert.ToInt32(Console.ReadLine());
+
+            result = (x >= y) ? x += 100 : y += 100;
+            
+            Console.WriteLine(result);
+
+
+
+        }
+    }
+}
---
+++ 17/TernaryOperator/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TernaryOperator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* Задание 2 
+Напишите программу, в которой получите от пользователя два целых числа в 
+переменные x и y. Для каждого из полученных чисел, используя тернарный оператор, 
+выполните проверку на то, является ли число четным, и если число является четным 
+– оставьте его значение неизменным, а если число является нечетным – увеличьте 
+его значение на 1.
+Создайте строковую переменную result, в которую присвойте результат выполнения 
+тернарного оператора. В этом тернарном операторе выполните проверку на то, 
+делится ли сума чисел, полученная в первой части задачи, на 4 без остатка. Если 
+сумма делится на 4 без остатка – переменной result присвойте фразу «Сумма 
+значений {x} и {y} делится на 4» (используйте интерполяцию строк). Если сумма на 4 
+не делится – присвойте фразу «Сумма значений {x} и {y} на 4 не делится». Выведите 
+значение result на экран */
+
+
+            Console.WriteLine("Enter your numbers: ");
+
+            int a, b, c, x, y;
+                        
+            x = Convert.ToInt32(Console.ReadLine());
+            y = Convert.ToInt32(Console.ReadLine());
+
+            a = x % 2;
+            b = y % 2;
+
+            x = (a == 0) ? x : ++x;
+            y = (b == 0) ? y : ++y;
+
+            c = (x + y) % 4;
+            
+            string result, value1, value2;
+
+            value1 =$"Summa of values {x} and {y} devided by 4";
+            value2 = $"Summa of values {x} and {y} not devided by 4";
+
+            result = (c == 0) ? value1 : value2;
+
+            Console.WriteLine(result);
+
+
+
+        }
+    }
+}
