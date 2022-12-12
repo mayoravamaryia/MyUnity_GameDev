ad597de0ae01cd3be391f116cd17c1cbe393c7e1
Maryia Mayorava <mayorava.maryia@gmail.com>
2022-12-12 11:00:10 +0100

    add HW arrays

---
+++ 19/001_Arrays/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Напишите программу, в которой создайте массив строковых элементов.В блоке
+инициализатора заполните массив названиями месяцев по порядку.Далее создайте
+цикл, в котором выведите на экран консоли все содержимое массива строк. */
+
+            string[] stringsArray = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+           
+            int index = 0;
+                
+            while (index <= 11)
+            {
+               string month = stringsArray[index];
+               Console.WriteLine(month);
+               index++;
+            }
+
+
+        }
+    }
+}
---
+++ 19/002_Arrays/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Напишите программу, в которой воспользуйтесь массивом строк из предыдущего задания. 
+Создайте цикл, в котором обратитесь к пользователю. Пользователь для первой 
+итерации цикла должен ввести строковое значение первого по порядку месяца, для 
+второй итерации – строковое значение второго месяца и так далее до конца массива. 
+Если пользователь введет неправильно текущее значение месяца – итерация цикла 
+должна повториться с тем, чтобы пользователь попробовал ввести значение еще 
+раз. Если пользователь ошибется при вводе значения 5го месяца – цикл должен 
+прекратить свою работу */
+
+
+            string[] monthList = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+            Console.Write("Enter ordinal first month: ");
+
+            string userMonth = Console.ReadLine();
+
+            int counter = 0;
+
+            while (counter < monthList.Length)
+            {
+                if (counter == 4 && userMonth != monthList[4])
+                {
+                    Console.WriteLine("Finsh!");
+                    break;
+                }
+                else if (userMonth == monthList[counter])
+                {
+                    counter++;
+                    Console.Write("Enter next month: ");
+                    userMonth = Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Enter true month!");
+                    Console.Write("Enter next month: ");
+                    userMonth = Console.ReadLine();
+                }
+            }
+        }
+    }
+}
+
