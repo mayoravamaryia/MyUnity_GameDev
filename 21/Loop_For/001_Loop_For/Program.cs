551186bf89a832ade5b7701950ee3cc291d31bef
Maryia Mayorava <mayorava.maryia@gmail.com>
2022-12-19 20:15:10 +0100

    add HW_lop_for

---
+++ 21/Loop_For/001_Loop_For/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_Loop_For
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Напишите программу, в которой, используя конструкции цикла со счетчиком:
+1. Выведите на экран консоли все числа от 0 до 100;
+2. Выведите на экран числа от 1001 до 2500 с шагом – 3;
+3. Выведите числа от 100 до 0 с шагом – 4;
+4. Выведите числа от 1 до 5 с шагом 0,2.
+ */
+            int length = 100;
+            for (int i = 0; i <= length; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            length = 2500;
+
+            for (int i = 1001; i <= length; i +=3 )
+            {
+                Console.WriteLine(i);
+            }
+
+            length = 0;
+            for (int i = 100; i >= length; i -=4)
+            {
+                Console.WriteLine(i);
+            }
+
+            float length1 = 5.0f;
+            for (float i = 1; i <= length1; i += 0.2f)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ 21/Loop_For/001_Loop_For/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("001_Loop_For")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("001_Loop_For")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0a847129-bddd-40c7-b4b2-08f3f1f8b707")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
